ca78c3f7c105d91c7bcac75c8a195f909ce8cc89
mdasifalijihat <mdasifalijihat@gmail.com>
2025-09-25 14:57:37 +0600

    added firabase .env

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router";
 import Layout from "../Layout/Layout";
 import Home from "../components/pages/home/Home";
 import Login from "../components/login/Login";
+import Register from "../components/login/Register";
 
 export const router = createBrowserRouter([
   {
@@ -11,6 +12,7 @@ export const router = createBrowserRouter([
     children: [
       { index: true, Component: Home },
       { path: "/login", Component: Login },
+      { path: "/register", Component: Register },
     ],
   },
 ]);
